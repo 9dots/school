3f6e46a191f2b8b7abe432a192b78311e1b878f7
Michael Weintraub <mweint@gmail.com>
2018-01-18 14:28:33 -0800

    school styles

--- src/app/Classes/Classes.js
+++ src/app/Classes/Classes.js
@@ -1,31 +1,38 @@
 import ClassList from './ClassList'
 import StudentList from 'app/StudentList'
 import PropTypes from 'prop-types'
-import Lesson from 'app/Lesson'
-import { Card, Layout } from 'antd'
+import lesson from 'app/Lesson'
+import { Collapse, Layout } from 'antd'
 import styles from 'theme/vars/vars.js'
-import React from 'react'
+import React, { Component } from 'react'
 import './Classes.less'
 
-const Classes = props => {
-  return (
-    <Layout>
-      <Layout.Sider width={styles['@sidebar-width']}>
-        <ClassList />
-      </Layout.Sider>
-      <Layout.Content
-        style={{
-          minHeight: 'calc(100vh - 64px)',
-          padding: 50,
-          backgroundColor: '#FAFAFA'
-        }}>
-        {lessons.map((lesson, i) => <Lesson lesson={lesson} key={i} />)}
-      </Layout.Content>
-      <Layout.Sider width={styles['@sidebar-width']}>
-        <StudentList />
-      </Layout.Sider>
-    </Layout>
-  )
+class Classes extends Component {
+  render () {
+    return (
+      <Layout>
+        <Layout.Sider width={styles['@sidebar-width']}>
+          <ClassList />
+        </Layout.Sider>
+        <Layout.Content
+          style={{
+            minHeight: 'calc(100vh - 64px)',
+            padding: 50,
+            backgroundColor: '#F7F7F7'
+          }}>
+          <Collapse
+            defaultActiveKey={['0']}
+            bordered={false}
+            className='lessons-collapse'>
+            {lessons.map((val, key) => lesson({ lesson: val, key }))}
+          </Collapse>
+        </Layout.Content>
+        <Layout.Sider width={styles['@sidebar-width']}>
+          <StudentList />
+        </Layout.Sider>
+      </Layout>
+    )
+  }
 }
 
 Classes.propTypes = {}
@@ -35,7 +42,7 @@ export default Classes
 const lessons = [
   {
     title: 'Myster Robot',
-    status: 'active'
+    assigned: true
   },
   {
     title: 'pixelBotGo'
@@ -54,5 +61,8 @@ const lessons = [
   },
   {
     title: 'J@m taC0 5alAd'
+  },
+  {
+    title: 'PixelBots'
   }
 ]
