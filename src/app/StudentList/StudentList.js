093e329ebe36b9776be4f9701e2bc9cd61bc6727
Michael Weintraub <mweint@gmail.com>
2018-01-22 12:18:27 -0800

    styling lessons

--- src/app/StudentList/StudentList.js
+++ src/app/StudentList/StudentList.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types'
-import { Menu, Avatar } from 'antd'
+import { Menu, Avatar, Progress } from 'antd'
 import React from 'react'
 import './StudentList.less'
 
 const StudentList = props => {
+  const h = '40px'
   return (
     <Menu
       mode='vertical-right'
@@ -12,13 +13,16 @@ const StudentList = props => {
         minHeight: 'calc(100vh - 62px)',
         paddingTop: 20
       }}>
-      {[1, 2, 3, 4, 5].map(i => (
-        <Menu.Item key={i} onClick={() => console.log(i)}>
-          <Avatar
-            size='small'
-            style={{ marginRight: 10, textAlign: 'center' }}
-            icon='user' />
+      {students.map(i => (
+        <Menu.Item
+          key={i}
+          onClick={() => console.log(i)}
+          style={{ margin: 0, lineHeight: h, height: h }}>
+          {/* <Avatar size='small' style={{ marginRight: 10, textAlign: 'center' }}>
+            {['A', 'B', 'E', 'C', 'Q', 'S'][Math.round(Math.random() * 6)]}
+          </Avatar> */}
           Student {i}
+          {/* <Progress type='circle' width={32} /> */}
         </Menu.Item>
       ))}
     </Menu>
@@ -28,3 +32,27 @@ const StudentList = props => {
 StudentList.propTypes = {}
 
 export default StudentList
+
+const students = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21
+]
