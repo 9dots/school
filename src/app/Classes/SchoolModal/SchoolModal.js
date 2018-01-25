3e8844c32b7ec050f7de364887c5a1656aabdb30
danleavitt0 <danleavitt0@gmail.com>
2018-01-25 12:43:45 -0800

    switch schools ;

--- src/app/Classes/SchoolModal/SchoolModal.js
+++ src/app/Classes/SchoolModal/SchoolModal.js
@@ -1,22 +1,72 @@
+import { Field, reduxForm, SubmissionError } from 'redux-form'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose, withHandlers } from 'recompose'
+import { SelectField } from 'redux-form-antd'
+import { connect } from 'react-redux'
+import { rpc, setUrl } from '../../actions'
+import { Modal, Form } from 'antd'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { Modal, Form, Input, AutoComplete } from 'antd'
 import './SchoolModal.less'
 
-const data = ['1', '2', '3', '4', '5']
+const enhancer = compose(
+  firestoreConnect([{ collection: 'schools', orderBy: ['displayName'] }]),
+  connect(
+    ({
+      firebase: { auth: { uid }, profile },
+      firestore: { ordered: { schools = [] } }
+    }) => ({
+      schools: schools.filter(s => !profile.schools[s.id]).map(school => ({
+        label: school.displayName,
+        value: school.id
+      })),
+      uid
+    })
+  ),
+  reduxForm({ form: 'createSchool' }),
+  withHandlers({
+    onSubmit: ({ uid, dispatch, history }) => values => {
+      return dispatch(
+        rpc('user.addToSchool', {
+          user: uid,
+          role: 'teacher',
+          ...values
+        })
+      )
+        .then(res => {
+          if (!res.ok) {
+            throw new Error(res.error)
+          }
+          return dispatch(setUrl(history, '/'))
+        })
+        .catch(e => {
+          console.warn(e)
+          throw new SubmissionError({
+            school: 'School code not found.'
+          })
+        })
+    }
+  })
+)
+const commonProps = {
+  validate: v => (v ? '' : 'Required')
+}
 
 const SchoolModal = props => {
   return (
-    <Modal {...props} title='Create A School'>
+    <Modal
+      {...props}
+      title='Join A School'
+      onOk={props.handleSubmit(props.onSubmit)}>
       <Form>
         <Form.Item>
-          <AutoComplete dataSource={data} size='large' />
-        </Form.Item>
-        <Form.Item>
-          <Input />
-        </Form.Item>
-        <Form.Item>
-          <Input />
+          <Field
+            {...commonProps}
+            name='school'
+            placeholder='Select a School'
+            options={props.schools}
+            notFoundContent='No schools to join'
+            component={SelectField} />
         </Form.Item>
       </Form>
     </Modal>
@@ -25,4 +75,4 @@ const SchoolModal = props => {
 
 SchoolModal.propTypes = {}
 
-export default SchoolModal
+export default enhancer(SchoolModal)
