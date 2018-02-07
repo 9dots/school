46c99ce1f841afba14734efed853c2002d42b59c
Michael Weintraub <mweint@gmail.com>
2018-02-07 14:29:10 -0800

    fixed modal rerouting on Course Card issue

--- src/app/Courses/CourseCard/CourseCard.js
+++ src/app/Courses/CourseCard/CourseCard.js
@@ -2,6 +2,7 @@ import modalContainer from 'components/modalContainer'
 import { Card, Icon, Button, Avatar } from 'antd'
 import AddCourseModal from 'app/AddCourseModal'
 import { stopEvent } from '../../../utils'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import React from 'react'
 import './CourseCard.less'
@@ -17,39 +18,45 @@ const CourseCard = ({ course, hideModal, showModal, isVisible }) => {
   } = course
 
   return (
-    <Card
-      className='course-card'
-      onClick={() => console.log('jam')}
-      bordered={false}
-      title={
-        <span>
-          <Avatar src={image} style={{ float: 'left' }}>
-            {title[0]}
-          </Avatar>
-          <span>{title}</span>
-        </span>
-      }
-      extra={<span />}>
-      <div className='course-description'>{description}</div>
-      <Card.Meta
-        description={
-          <span>
-            <Icon type='tag-o' style={{ marginRight: '5px' }} />
-            {tags.join(', ')}
-            <Icon type='book' style={{ margin: '0 5px 0 10px' }} />
-            {difficulty}
-            <Icon type='clock-circle-o' style={{ margin: '0 5px 0 10px' }} />
-            {duration.time} {duration.unit}
-          </span>
-        } />
-      <Button onClick={stopEvent(showModal('addCourse'))}>
-        <Icon type='plus' />Add to Class
-      </Button>
+    <span>
+      <Link to='courses/coursId'>
+        <Card
+          className='course-card'
+          bordered={false}
+          title={
+            <span>
+              <Avatar src={image} style={{ float: 'left' }}>
+                {title[0]}
+              </Avatar>
+              <span>{title}</span>
+            </span>
+          }
+          extra={
+            <Button onClick={stopEvent(showModal('addCourse'))}>
+              <Icon type='plus' />Add to Class
+            </Button>
+          }>
+          <div className='course-description'>{description}</div>
+          <Card.Meta
+            description={
+              <span>
+                <Icon type='tag-o' style={{ marginRight: '5px' }} />
+                {tags.join(', ')}
+                <Icon type='book' style={{ margin: '0 5px 0 10px' }} />
+                {difficulty}
+                <Icon
+                  type='clock-circle-o'
+                  style={{ margin: '0 5px 0 10px' }} />
+                {duration.time} {duration.unit}
+              </span>
+            } />
+        </Card>
+      </Link>
       <AddCourseModal
         onOk={hideModal('addCourse')}
         onCancel={hideModal('addCourse')}
         visible={isVisible('addCourse')} />
-    </Card>
+    </span>
   )
 }
 
