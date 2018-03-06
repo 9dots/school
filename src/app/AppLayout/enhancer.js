2f359035d0f75466cf196331b5f101d200aafb0b
Michael Weintraub <mweint@gmail.com>
2018-03-05 16:40:21 -0800

    Started AppLayout and student views

---
+++ src/app/AppLayout/enhancer.js
@@ -0,0 +1,22 @@
+import { firebaseConnect } from 'react-redux-firebase'
+import { compose, withHandlers } from 'recompose'
+import { connect } from 'react-redux'
+
+export default compose(
+  firebaseConnect(),
+  connect(({ firebase: { profile } }) => ({
+    profile
+  })),
+  withHandlers({
+    logout: props => event => {
+      switch (event.key) {
+        case 'logout':
+          return props.firebase
+            .logout()
+            .then(() => (window.location = window.location.origin))
+        default:
+          break
+      }
+    }
+  })
+)
