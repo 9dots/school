c656eb35098a18f805073adea665b4596e5c1c78
Michael Weintraub <mweint@gmail.com>
2018-03-08 13:29:07 -0800

    sudent links in courses

---
+++ src/app/Course/LessonList/LessonItem/Readme.md
@@ -0,0 +1,5 @@
+# <Lesson Item/>
+
+
+
+## Usage
---
+++ src/app/Course/LessonList/Tasks/Tasks.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import './Tasks.less'
+
+const Tasks = (props) => {
+  return (
+      
+  )
+}
+
+Tasks.propTypes = {
+
+}
+
+export default Tasks
