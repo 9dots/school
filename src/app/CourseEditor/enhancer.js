fcde5e73950d76ece1f24c1962722950ff82a3de
danleavitt0 <danleavitt0@gmail.com>
2018-04-12 10:16:06 -0700

    drag and drop

--- src/app/CourseEditor/enhancer.js
+++ src/app/CourseEditor/enhancer.js
@@ -1,10 +1,14 @@
+import { compose, withState, lifecycle, withHandlers } from 'recompose'
 import { firestoreConnect } from 'react-redux-firebase'
 import modalContainer from 'components/modalContainer'
-import { compose, withState } from 'recompose'
 import waitFor from '../../components/waitFor'
+import { setUrl, rpc } from 'app/actions'
+import { setArrayImmutable } from 'utils'
 import { course } from '../../selectors'
+import deepEqual from '@f/deep-equal'
 import { connect } from 'react-redux'
-import { setUrl } from 'app/actions'
+import splice from '@f/splice'
+import { message } from 'antd'
 
 export default compose(
   modalContainer,
@@ -12,10 +16,48 @@ export default compose(
     (state, { match: { params: { courseId } } }) => ({
       courseId
     }),
-    { setUrl }
+    { setUrl, rpc }
   ),
   withState('editKey', 'setEditKey', null),
   withState('mode', 'setMode', 'edit'),
+  withState('orderedLessons', 'setOrderedLessons', []),
+  withHandlers({
+    sendReorder: props => async data => {
+      try {
+        await props.rpc('course.reorder', {
+          course: props.courseId,
+          ...data
+        })
+      } catch (e) {
+        console.log(e)
+        message.error(e)
+      }
+    }
+  }),
+  withHandlers({
+    onDrop: ({ orderedLessons, setOrderedLessons, sendReorder }) => e => {
+      const { reason, destination, source, draggableId, type } = e
+      if (reason === 'DROP' && destination) {
+        sendReorder({
+          source,
+          type,
+          destination,
+          id: draggableId
+        })
+        if (type === 'lesson') {
+          const ordered = reorder(
+            orderedLessons,
+            source.index,
+            destination.index
+          )
+          return setOrderedLessons(ordered)
+        }
+        return setOrderedLessons(
+          handleDrop(orderedLessons, source, destination, draggableId)
+        )
+      }
+    }
+  }),
   firestoreConnect(props => [
     {
       collection: 'courses',
@@ -24,5 +66,49 @@ export default compose(
     }
   ]),
   connect((state, props) => ({ course: course(state, props.courseId) })),
-  waitFor(['course'])
+  waitFor(['course']),
+  lifecycle({
+    componentWillUpdate (nextProps) {
+      if (!this.props.isLoaded && nextProps.isLoaded) {
+        this.props.setOrderedLessons(nextProps.course.lessons)
+      }
+      if (!deepEqual(this.props.course, nextProps.course)) {
+        this.props.setOrderedLessons(nextProps.course.lessons)
+      }
+    }
+  })
 )
+
+function handleDrop (arr, source, destination, id) {
+  const next = arr.find(item => item.id === destination.droppableId)
+  const prev = arr.find(item => item.id === source.droppableId)
+  if (source.droppableId === destination.droppableId) {
+    return setArrayImmutable(arr, arr.indexOf(next), {
+      ...next,
+      tasks: reorder(next.tasks, source.index, destination.index)
+    })
+  }
+  const target = prev.tasks.find(t => t.id === id)
+  return arr.map(lesson => {
+    if (lesson.id === destination.droppableId) {
+      return {
+        ...lesson,
+        tasks: splice(lesson.tasks, destination.index, 0, target)
+      }
+    } else if (lesson.id === source.droppableId) {
+      return {
+        ...lesson,
+        tasks: splice(lesson.tasks, source.index, 1)
+      }
+    }
+    return lesson
+  })
+}
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list)
+  const [removed] = result.splice(startIndex, 1)
+  result.splice(endIndex, 0, removed)
+
+  return result
+}
