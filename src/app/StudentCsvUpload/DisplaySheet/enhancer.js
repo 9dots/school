e4ae366217fe83d6381e26ee55754153c71892cd
Michael Weintraub <mweint@gmail.com>
2018-05-22 18:02:30 -0700

    csv uploading QA

--- src/app/StudentCsvUpload/DisplaySheet/enhancer.js
+++ src/app/StudentCsvUpload/DisplaySheet/enhancer.js
@@ -1,25 +1,39 @@
 import { compose, withHandlers } from 'recompose'
 import formModal from 'components/formModal'
+import { connect } from 'react-redux'
 import mapValues from '@f/map-values'
 import schema from 'school-schema'
+import { rpc } from 'app/actions'
 import { validate } from 'utils'
 import { message } from 'antd'
 import omit from '@f/omit'
 
 export default compose(
+  connect(null, { rpc }),
   formModal({
     displayName: 'createStudents',
     mapPropsToValues: ({ data = {} }) => ({
       ...data
     }),
     handleSubmit: async (values, { props, setErrors }) => {
-      console.log(values)
-      // const {
-      //   class: { id },
-      //   setLoading,
-      //   rpc
-      // } = props
-      // setLoading(true)
+      const {
+        class: { id },
+        setLoading,
+        rpc
+      } = props
+      setLoading(true)
+      try {
+        const { add = [] } = await rpc(
+          'user.createStudents',
+          cast(values, props)
+        )
+        const students = add.filter((val, i) => values[i] !== true)
+        await rpc('class.addStudents', { students, class: id })
+        props.onOk(null)
+      } catch (e) {
+        setLoading(false)
+        message.error('Oops, something went wrong. Please try again.')
+      }
     },
     validateOnChange: false,
     validateOnBlur: true,
